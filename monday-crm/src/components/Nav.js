5bd0237519d2def7790ebfda078f5acd07219edf
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-08-06 23:03:29 -0300

    Criando a estrutura do projeto

---
+++ monday-crm/src/components/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return (
+        <nav>
+            <div className="logo-container">
+                <img src="{logo}" alt="logo"/>
+            </div>
+        </nav>
+    )
+}
